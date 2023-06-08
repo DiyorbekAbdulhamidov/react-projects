171d3c13045030f13ca051249c225f98b1ece09f
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-06-08 15:09:42 +0500

    section5

--- src/info/section5/section5.jsx
+++ src/info/section5/section5.jsx
@@ -19,3 +19,28 @@ class Section5 extends React.Component {
 }
 
 export default Section5;
+
+
+
+
+// import React from "react";
+// import "./section5.scss";
+// import boy from "../../assets/imgs/boy.svg";
+
+
+// class Section5 extends React.Component {
+//   render() {
+//     return (
+//       <section className="section5">
+//         <img src={boy} alt="" />
+//         <div className="sub">
+//           <h4>Contact Us</h4>
+//           <span>Discover your current English level by taking our free online English test.Sign <br /> up to our newsletter for learning tips and free resources</span>
+//           <input type="text" placeholder="Enter Your E-mail"/> <button>Subscribe</button>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
+
+// export default Section5;
